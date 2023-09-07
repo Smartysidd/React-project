497424c13084ec32567a24c1d6f0f7b88eaadebc
Smartysidd <Sidd271299@gmail.com>
2023-09-07 18:39:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,53 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import AboutUs from './components/AboutUs';
-import Contact from './components/Contact';
-import SignIn from './components/SignIn';
-import Courses from './components/Courses';
-import Home from './components/Home';
-import  {useState}from 'react';
-import { BrowserRouter,Routes,Route,Link} from 'react-router-dom';
-
-
-// function Home(){
-//   return <h1>Home</h1>;
-//   }
-
-
-// function AboutUs(){
-// return <h1>AboutUs</h1>;
-// }
-
-// function Courses(){
-//   return <h1>Courses</h1>;
-//   }
-
-//   function Contact(){
-//     return <h1>Contact</h1>;
-//     }
-
-//     function SignIn(){
-//       return <h1>SignIn</h1>
-//     }
-
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import necessary components for routing
+import NavBar from './NavBar'; // Make sure to import your NavBar and other components
+import Home from './Home';
+import AboutUs from './AboutUs';
+import Courses from './Courses'; // You should import your Courses component
+import Contact from './Contact';
+import SignIn from './SignIn';
 
 function App() {
-  const [CartItems,setCartItems]=useState([]);
-  const courses=[
-    { id:1,name:"course 1", price:50},
-    { id:2,name:"course 2", price:75},
-    { id:3,name:"course 3", price:100},];
-    const AddToCart=(course)=>{
-      setCartItems([...CartItems,course]);
-    };
-  
-  return (
-    <div className="App">
-      <h1>online Courses</h1><Courses courses={courses} onAddTOCart={addToCart}/>
-      <div className="cart">
-        <h2>Shopping Cart</h2>
-        <ul>
-          {cartItems.map((item)=>(<li key={item.id}>
-            {item.name}-${item.price}
-            </li>
-            ))}
-        </ul>
-      </div>
-    </div>);
-    
-    <BrowserRouter>
-    <NavBar/>
-    {/* <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/AboutUs">AboutUs</Link>
-        </li>
-        <li>
-          <Link to="/Courses">Courses</Link>
-        </li>
-        <li>
-          <Link to="/Contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/SignIn">SignIn</Link>
-        </li>
-      </ul>
-    </nav> */}
-    <Routes>
-    <Route Path='/'element={<Home/>}></Route>
-      <Route Path='/AboutUs'element={<AboutUs/>}></Route>
-      <Route Path='/Courses'element={<Courses/>}></Route>
-      <Route path='/Contact'element={<Contact/>}></Route>
-      <Route path='/SignIn'element={<SignIn/>}></Route>
-    </Routes>
-    </BrowserRouter>
-    
- 
+  const [cartItems, setCartItems] = useState([]);
+  const courses = [
+    { id: 1, name: "course 1", price: 50 },
+    { id: 2, name: "course 2", price: 75 },
+    { id: 3, name: "course 3", price: 100 },
+  ];
 
+  const addToCart = (course) => {
+    setCartItems([...cartItems, course]);
+  };
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="App">
+        <h1>Online Courses</h1>
+        {/* Pass the courses and addToCart function to the Courses component */}
+        <Courses courses={courses} onAddToCart={addToCart} />
+        <div className="cart">
+          <h2>Shopping Cart</h2>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id}>
+                {item.name} - ${item.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/Courses" element={<Courses />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,133 +1,63 @@
-// import React, { Component } from 'react';
-
-// class SignIn extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: '',
-//       password: '',
-//     };
-//   }
-
-//   handleChange = (e) => {
-//     this.setState({ [e.target.name]: e.target.value });
-//   };
-
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     const { username, password } = this.state;
-//     // Handle sign-in logic here (e.g., send data to a server, check credentials)
-//     console.log('Sign-in submitted:', { username, password });
-//     // You can also clear the form fields or perform any other necessary actions
-//     this.setState({
-//       username: '',
-//       password: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>Sign In</h2>
-//         <form onSubmit={this.handleSubmit}>
-//           <div>
-//             <label htmlFor="username">Username/Email:</label>
-//             <input
-//               type="text"
-//               id="username"
-//               name="username"
-//               value={this.state.username}
-//               onChange={this.handleChange}
-//               required
-//             />
-//           </div>
-//           <div>
-//             <label htmlFor="password">Password:</label>
-//             <input
-//               type="password"
-//               id="password"
-//               name="password"
-//               value={this.state.password}
-//               onChange={this.handleChange}
-//               required
-//             />
-//           </div>
-//           <button type="submit">Sign In</button>
-//         </form>
-//         <p>
-//           <a href="/forgot-password">Forgot Password?</a>
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default SignIn;
-
-
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './SignIn.css';
 
-class SignIn extends Component {
-  constructor() {
-    super();
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { username, password } = this.state;
-    // Handle sign-in logic here (e.g., send data to a server, check credentials)
+    const { username, password } = formData;
     console.log('Sign-in submitted:', { username, password });
-    // You can also clear the form fields or perform any other necessary actions
-    this.setState({
+
+    setFormData({
       username: '',
       password: '',
     });
   };
 
-  render() {
-    return (
-      <div className="sign-in-container">
-        <h2>Sign In</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username/Email:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Sign In</button>
-        </form>
-        <p>
-          <a href="/forgot-password">Forgot Password?</a>
-        </p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="sign-in-container">
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username/Email:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+      <p>
+        <a href="/forgot-password">Forgot Password?</a>
+      </p>
+    </div>
+  );
+};
 
 export default SignIn;
